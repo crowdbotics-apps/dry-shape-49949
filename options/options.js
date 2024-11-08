11445c8ab1d857a178c4b889912c036d0fc13d45
Crowdbotics <team@crowdbotics.com>
2024-11-08 13:35:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-shape-49949",
+  url: "https://dry-shape-49949.botics.co",
+  api: "https://dry-shape-49949.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
